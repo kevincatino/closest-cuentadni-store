550873048c61937e8a35b7e9dc648b8efee04971
khcatino <kcatino@itba.edu.ar>
2023-08-06 11:07:02 -0300

    Store service initial implementation

--- cmd/closest-cuentadni-store/main.go
+++ cmd/closest-cuentadni-store/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"math"
+	"os"
+)
+
+type (
+	Coordinates struct {
+		lat  float64
+		long float64
+	}
+
+	Store struct {
+		name        string
+		coordinates Coordinates
+		address     string
+		localidad   string
+	}
+)
+
+func (c Coordinates) Lat() float64 {
+	return c.lat
+}
+
+func (c Coordinates) Long() float64 {
+	return c.long
+}
+
+func (c Coordinates) GetDistance(other Coordinates) float64 {
+	return math.Sqrt(math.Pow(c.Lat()-other.Lat(), 2) + math.Pow(c.Long()-other.Long(), 2))
+}
+
 func main() {
-	print("hola")
+	localidad := os.Getenv("LOCALIDAD")
+	address := os.Getenv("DIRECCION")
 }
